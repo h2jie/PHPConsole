f2ced176587f9b96e88ce3e7f9b0e30bc97b5b8e
h2jie <h2jie09@gmail.com>
2018-03-04 17:33:03 +0100

    proyecto finalizado

--- console_result.php
+++ console_result.php
@@ -1,26 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <style>
+    ::-webkit-scrollbar
+    {
+      width: 12px;  / for vertical scrollbars /
+    height: 12px; / for horizontal scrollbars /
+    }
 
-<div style="color:white;font-family: 'Source Code Pro', monospace;">
-  <?php
-  include 'constants.inc.php';
+    ::-webkit-scrollbar-track
+    {
+      background: rgb(34, 34, 34);
+    }
 
-  session_start();
-  $multiple_response = Array();
-  if (!empty($_SESSION['answer'])) {
-      if (is_array($_SESSION['answer'])) {
-          $multiple_response = $_SESSION['answer'];
+    ::-webkit-scrollbar-thumb
+    {
+      background: rgb(97, 97, 97);
+    }
+  </style>
+  <body>
 
-          foreach ($multiple_response as $item) {
-              echo $item . "<br>";
+    <div style="color:white;font-family: 'Source Code Pro', monospace;">
+      <?php
+      include 'constants.inc.php';
+
+      $result ="";
+      session_start();
+      $multiple_response = Array();
+      if (!empty($_SESSION['answer'])) {
+          if (is_array($_SESSION['answer'])) {
+              $multiple_response = $_SESSION['answer'];
+
+              foreach ($multiple_response as $item) {
+                  echo $item . "<br>";
+
+              }
+          } else {
+              $single_response = $_SESSION['answer'];
+              echo $single_response;
+
+
           }
-      } else {
-          $single_response = $_SESSION['answer'];
-          echo $single_response;
+          session_destroy();
+      }else{
+          echo 'Introduce comando <span style="color:#990000">help</span> para obtener la guia.';
+
       }
-      session_destroy();
-  }else{
-      echo 'Introduce comando <span style="color:#990000">help</span> para obtener la guia.';
 
-  }
-
-  ?>
-</div>
+      ?>
+    </div>
+  </body>
+</html>
